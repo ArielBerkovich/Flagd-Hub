5af7c159cecfed76ef27aeb9c1d4489d7b2ee96f
bg <barakgonen9@gmail.com>
2025-01-11 17:07:59 +0200

    Init commit

--- flagd-hub-server/flagd-hub-api/src/main/java/org/flagd/hub/config/server/services/FlagdConfigurationService.java
+++ flagd-hub-server/flagd-hub-api/src/main/java/org/flagd/hub/config/server/services/FlagdConfigurationService.java
@@ -3,19 +3,23 @@ package org.flagd.hub.config.server.services;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
 import org.flagd.hub.config.server.FlagdConfigurationConverter;
-import org.flagd.hub.config.server.repositories.FeatureFlagsRepository;
+import org.flagd.hub.config.server.repositories.FeatureFlagRepository;
 import org.flagd.hub.rest.model.FeatureFlag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class FlagdConfigurationService {
-    private final FeatureFlagsRepository featureFlagsRepository;
-
-    public ObjectNode getFlagdConfiguration() {
-        List<FeatureFlag> allFlags = featureFlagsRepository.getAll();
-        return FlagdConfigurationConverter.convertToFlagdFormat(allFlags);
-    }
-}
+//@Service
+//@RequiredArgsConstructor
+//public class FlagdConfigurationService {
+//    @Autowired
+//    private final FeatureFlagRepository featureFlagRepository;
+//
+//    public ObjectNode getFlagdConfiguration() {
+//        List<FeatureFlag> allFeatureFlags = new ArrayList<>();
+//        featureFlagRepository.findAll().forEach(allFeatureFlags::add);
+//        return FlagdConfigurationConverter.convertToFlagdFormat(allFeatureFlags);
+//    }
+//}
