fb97d22e827a658719e81fb754f9db0020ecddf8
ArielBerkovich <34457341+ArielBerkovich@users.noreply.github.com>
2025-01-18 18:26:23 +0200

    fixed dashboard adding new flags automatically

--- flagd-hub-ui/src/pages/dashborad.tsx
+++ flagd-hub-ui/src/pages/dashborad.tsx
@@ -58,8 +58,7 @@ const Dashboard: React.FC<DashboardProps> = ({ activeArea }) => {
 
   const handleCreateFlag = (newFlag: FeatureFlag) => {
     FeatureFlagService.createFeatureFlag(newFlag);
-    setFeatureFlags(prev => [...prev, newFlag]);
-    setIsPopupOpen(false); // Close popup after creation
+    setIsPopupOpen(false);
   };
 
   const openExportPopup = async () => {
